16b11d4a1e8ebef4cb2691458b90bf2a488c9705
landod <landod@rose-hulman.edu>
2018-10-28 01:32:52 -0400

    Finished exam 1 Hour and 10 minutes

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Owen Land.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,9 +90,19 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    y = abs(sequence[0])
+    z = 0
+    for k in range(1, len(sequence)):
+        x = abs(sequence[k])
+
+        if x > y:
+            y = x
+            z = k
+
+    return z
 
 
 ###############################################################################
